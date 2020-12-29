aebd7cb14d9e9ecbb92161f193bb3bc6225dab94
Markus Thielker <markus.thielker@uni-ulm.de>
2020-12-29 20:14:53 +0100

    GomokuField.kt -> GomokuFieldColor.kt
    GomokuField is now the node of the newly implemented line tracing system.

--- core/src/de/markus_thielker/gomoku/components/GomokuField.kt
+++ core/src/de/markus_thielker/gomoku/components/GomokuField.kt
@@ -1,16 +1,42 @@
 package de.markus_thielker.gomoku.components
 
 /**
- * This enum defines the state of a gomoku field
- *
- * Black - The field is already blocked by a black stone
- * White - The field is already blocked by a white stone
- * None  - The field is not blocked yet
+ * Field and line connection algorithm for fast evaluation of links and lengths
  *
  * @author Markus Thielker
  *
  * */
-enum class GomokuField {
+class GomokuField(val pos : Array<Int>, var color : GomokuFieldColor) {
 
-    Black, White, None
+    val con = HashMap<GomokuFieldDirection, GomokuField?>()
+    val conLen = HashMap<GomokuFieldDirection, Int>()
+
+    init {
+        conLen[GomokuFieldDirection.Horizontal] = 1
+        conLen[GomokuFieldDirection.Vertical] = 1
+        conLen[GomokuFieldDirection.DiagonalTLBR] = 1
+        conLen[GomokuFieldDirection.DiagonalBLTR] = 1
+    }
+
+    /**
+     * This function prints the current state of the field with all information
+     * It is used for debugging only and does not need to be transferred to main project
+     *
+     * @author Markus Thielker
+     *
+     * */
+    fun printField() {
+
+        println()
+
+        println("name: $this")
+        println("position: ${pos.contentToString()}")
+        println("color: $color")
+        println("connections:")
+        println("  - Horizontal: ${con[GomokuFieldDirection.Horizontal]} | ${conLen[GomokuFieldDirection.Horizontal]}")
+        println("  - Vertical: ${con[GomokuFieldDirection.Vertical]} | ${conLen[GomokuFieldDirection.Vertical]}")
+        println("  - DiagonalTLBR: ${con[GomokuFieldDirection.DiagonalTLBR]} | ${conLen[GomokuFieldDirection.DiagonalTLBR]}")
+        println("  - DiagonalBLTR: ${con[GomokuFieldDirection.DiagonalBLTR]} | ${conLen[GomokuFieldDirection.DiagonalBLTR]}")
+        println()
+    }
 }
---
+++ core/src/de/markus_thielker/gomoku/components/GomokuFieldColor.kt
@@ -0,0 +1,15 @@
+package de.markus_thielker.gomoku.components
+
+/**
+ * This enum defines the state of a gomoku field
+ *
+ * Black - The field is already blocked by a black stone
+ * White - The field is already blocked by a white stone
+ *
+ * @author Markus Thielker
+ *
+ * */
+enum class GomokuFieldColor {
+
+    Black, White
+}
