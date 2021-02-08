a18717a2faf7020bf3aed9d01a28f3892fe2fa07
Markus Thielker <markus.thielker@uni-ulm.de>
2021-02-08 23:31:35 +0100

    feat: added a requestPing-Test for client side

--- core/src/test/kotlin/de/markus_thielker/gomoku/components/SimpleClientTest.kt
+++ core/src/test/kotlin/de/markus_thielker/gomoku/components/SimpleClientTest.kt
@@ -34,4 +34,42 @@ class SimpleClientTest {
         // close connection by sending goodbye to server
         client.closeSession()
     }
+
+    @Test
+    fun sendPingTest() {
+
+        val address = "localhost"
+        val port = 42002
+
+        val server = TestServer(InetSocketAddress(address, port))
+        server.start()
+
+        Thread.sleep(500)
+
+        val opening = GomokuOpening.Standard
+        val p1 = GomokuPlayer("Player 1", GomokuFieldColor.Black)
+        val p2 = GomokuPlayer("Player 2", GomokuFieldColor.White)
+
+        val game = GomokuGame(GameView(Application(), opening, p1, p2), opening, p1, p2)
+
+        // create client socket focusing on local server
+        val client = SimpleClient(URI(String.format("ws://%s:%d", address, port)), game)
+
+        // connect to passed connection
+        client.connect()
+
+        Thread.sleep(500)
+
+        client.requestPing()
+
+        // timeout to ensure connection
+        Thread.sleep(500)
+
+        // close connection by sending goodbye to server
+        client.closeSession()
+
+        Thread.sleep(1000)
+
+        assert(server.connectionOpened && server.connectionClosed && !server.exceptionOccurred && server.messagesReceived.isNotEmpty())
+    }
 }
