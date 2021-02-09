5c6152842448091a0fe79ee9853a1d74a0b645b5
Markus Thielker <markus.thielker@uni-ulm.de>
2021-02-09 16:54:09 +0100

    feat: enlarged GomokuPlayer test cases

--- core/src/test/kotlin/de/markus_thielker/gomoku/components/GomokuPlayerTest.kt
+++ core/src/test/kotlin/de/markus_thielker/gomoku/components/GomokuPlayerTest.kt
@@ -5,14 +5,47 @@ import org.junit.Test
 class GomokuPlayerTest {
 
     @Test
-    fun updateStateTest() {
+    fun updateStatePlacedTest() {
 
         val player = GomokuPlayer("Player", GomokuFieldColor.Black)
 
-        player.updateState(arrayOf(1, 1), 1, false)
-        player.updateState(arrayOf(1, 2), 2, true)
+        player.updateState(arrayOf(1, 1), 2, null)
 
-        assert(player.stones.isNotEmpty() && player.maximum == 2 && player.placed == 2 && player.wins == 1 && player.streak == 1)
+        assert(player.stones.isNotEmpty() && player.maximum == 2 && player.placed == 1 && player.wins == 0 && player.streak == 0)
+    }
+
+    @Test
+    fun updateStateWinTest() {
+
+        val player = GomokuPlayer("Player", GomokuFieldColor.Black)
+
+        player.updateState(arrayOf(1, 1), 2, true)
+
+        assert(player.stones.isNotEmpty() && player.maximum == 2 && player.placed == 1 && player.wins == 1 && player.streak == 1)
+    }
+
+    @Test
+    fun updateStateLoseTest() {
+
+        val player = GomokuPlayer("Player", GomokuFieldColor.Black)
+
+        player.updateState(arrayOf(1, 1), 2, false)
+
+        assert(player.stones.isNotEmpty() && player.maximum == 2 && player.placed == 1 && player.wins == 0 && player.streak == 0)
+    }
+
+    @Test
+    fun updateStateStreakResetTest() {
+
+        val player = GomokuPlayer("Player", GomokuFieldColor.Black)
+
+        player.updateState(arrayOf(1, 1), 2, true)
+
+        player.clearStats()
+
+        player.updateState(arrayOf(1, 1), 2, false)
+
+        assert(player.stones.isNotEmpty() && player.maximum == 2 && player.placed == 1 && player.wins == 1 && player.streak == 0)
     }
 
     @Test
