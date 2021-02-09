3a959b076214cfd2871e2b5e8a91d92d6be2e04d
Markus Thielker <markus.thielker@uni-ulm.de>
2021-02-09 16:55:40 +0100

    feat: added/changed to test win condition to all four directions

--- core/src/test/kotlin/de/markus_thielker/gomoku/components/GomokuGameTest.kt
+++ core/src/test/kotlin/de/markus_thielker/gomoku/components/GomokuGameTest.kt
@@ -21,7 +21,7 @@ class GomokuGameTest {
     }
 
     @Test
-    fun checkWinConditionTest() {
+    fun checkWinConditionHorizontalTest() {
 
         val opening = GomokuOpening.Standard
         val p1 = GomokuPlayer("Player 1", GomokuFieldColor.Black)
@@ -49,6 +49,96 @@ class GomokuGameTest {
 
         game.stonePlaced(4, 5)
 
-        assert(game.gameOver && game.winnerPlayer != null)
+        assert(game.gameOver && game.winnerPlayer == game.playerOne)
+    }
+
+    @Test
+    fun checkWinConditionVerticalTest() {
+
+        val opening = GomokuOpening.Standard
+        val p1 = GomokuPlayer("Player 1", GomokuFieldColor.Black)
+        val p2 = GomokuPlayer("Player 2", GomokuFieldColor.White)
+
+        val game = GomokuGame(GameView(Application(), opening, p1, p2), opening, p1, p2)
+
+        game.stonePlaced(1, 6)
+
+        game.stonePlaced(2, 6)
+
+        game.stonePlaced(1, 5)
+
+        game.stonePlaced(2, 5)
+
+        game.stonePlaced(1, 4)
+
+        game.stonePlaced(2, 4)
+
+        game.stonePlaced(1, 3)
+
+        game.stonePlaced(2, 3)
+
+        game.stonePlaced(1, 2)
+
+        assert(game.gameOver && game.winnerPlayer == game.playerOne)
+    }
+
+    @Test
+    fun checkWinConditionDiagonalTLBRTest() {
+
+        val opening = GomokuOpening.Standard
+        val p1 = GomokuPlayer("Player 1", GomokuFieldColor.Black)
+        val p2 = GomokuPlayer("Player 2", GomokuFieldColor.White)
+
+        val game = GomokuGame(GameView(Application(), opening, p1, p2), opening, p1, p2)
+
+        game.stonePlaced(1, 1)
+
+        game.stonePlaced(1, 2)
+
+        game.stonePlaced(2, 2)
+
+        game.stonePlaced(2, 3)
+
+        game.stonePlaced(3, 3)
+
+        game.stonePlaced(3, 4)
+
+        game.stonePlaced(4, 4)
+
+        game.stonePlaced(4, 5)
+
+        game.stonePlaced(5, 5)
+
+        assert(game.gameOver && game.winnerPlayer == game.playerOne)
+    }
+
+    @Test
+    fun checkWinConditionDiagonalBLTR() {
+
+        val opening = GomokuOpening.Standard
+        val p1 = GomokuPlayer("Player 1", GomokuFieldColor.Black)
+        val p2 = GomokuPlayer("Player 2", GomokuFieldColor.White)
+
+        val game = GomokuGame(GameView(Application(), opening, p1, p2), opening, p1, p2)
+
+        game.stonePlaced(1, 5)
+
+        game.stonePlaced(1, 6)
+
+        game.stonePlaced(2, 4)
+
+        game.stonePlaced(2, 5)
+
+        game.stonePlaced(3, 3)
+
+        game.stonePlaced(3, 4)
+
+        game.stonePlaced(4, 2)
+
+        game.stonePlaced(4, 3)
+
+        game.stonePlaced(5, 1)
+
+        assert(game.gameOver && game.winnerPlayer == game.playerOne)
     }
 }
